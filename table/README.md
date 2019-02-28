5a44a033de6043bfe08afa5719122a1cc84292b0
Cheng Zhao <zcbenz@gmail.com>
2019-02-28 14:54:42 +0900

    Add example for Table

---
+++ table/README.md
@@ -0,0 +1,12 @@
+# Table
+
+Example of `Table` with custom table model and custom cell drawing.
+
+|  macOS            |    Linux          |  Windows          |
+| ----------------- | ----------------- | ----------------- |
+| ![][mac-table]  | ![][linux-table] | ![][win-table]  |
+
+[mac-table]: https://cdn.jsdelivr.net/gh/yue/yue-sample-apps@40010ab/table/screenshots/mac_table.png
+[linux-table]: https://cdn.jsdelivr.net/gh/yue/yue-sample-apps@40010ab/table/screenshots/linux_table.png
+[win-table]: https://cdn.jsdelivr.net/gh/yue/yue-sample-apps@40010ab/table/screenshots/win_table.png
+
---
+++ table/js/table.js
@@ -0,0 +1,53 @@
+const gui = require('gui')
+
+const win = gui.Window.create({})
+win.setContentSize({width: 400, height: 400})
+win.onClose = () => gui.MessageLoop.quit()
+
+const model = gui.AbstractTableModel.create(3)
+model.getRowCount = (self) => {
+  return 10000
+}
+
+const editedText = {}
+model.getValue = (self, column, row) => {
+  if (column == 0) {
+    return String(row)
+  } else if (column == 1) {
+    if (editedText[row])
+      return editedText[row]
+    else
+      return `Edit Me ${row}`
+  } else if (column == 2) {
+    const r = (row % 5) * 40 + (row % 55) * 1
+    const g = (row % 4) * 40 + (row % 20) * 4
+    const b = (row % 3) * 40 + (row % 10) * 13
+    return {name: `rgb(${r}, ${g}, ${b})`, value: gui.Color.rgb(r, g, b)}
+  }
+}
+model.setValue = (self, column, row, value) => {
+  if (column == 1)
+    editedText[row] = value
+}
+
+const onDraw = (painter, rect, value) => {
+  painter.setFillColor(value.value)
+  painter.fillRect(rect)
+  painter.drawText(value.name, rect, {color: '#FFF'})
+}
+
+const table = gui.Table.create()
+table.addColumnWithOptions('ID', {width: 40})
+table.addColumnWithOptions('Editable Text', {type: 'edit', width: 100})
+table.addColumnWithOptions('Custom Color', {type: 'custom', onDraw})
+table.setModel(model)
+
+win.setContentView(table)
+
+win.center()
+win.activate()
+
+if (!process.versions.yode) {
+  gui.MessageLoop.run()
+  process.exit(0)
+}
